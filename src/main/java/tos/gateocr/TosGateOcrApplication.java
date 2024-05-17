eab88583286a8016c74856c1fe4132087607cf31
mkrimou <krimou.mohamed@gmail.com>
2024-05-17 12:01:05 +0100

    project init

---
+++ src/main/java/tos/gateocr/TosGateOcrApplication.java
@@ -0,0 +1,13 @@
+package tos.gateocr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TosGateOcrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TosGateOcrApplication.class, args);
+	}
+
+}
